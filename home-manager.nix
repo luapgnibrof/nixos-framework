d541c1ab3eb184743d1637603f75b00ce8c282f3
Luap Gnibrof <4041644+luapgnibrof@users.noreply.github.com>
2025-09-20 13:27:27 -0400

    Update home-manager.nix

--- home-manager.nix
+++ home-manager.nix
@@ -1,26 +1,112 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
-let
-  user = builtins.getEnv "USER"; # not used; HM binds per user below
-in
 {
-  # Attach Home Manager config to your user:
+  # Home Manager as a NixOS module: bind config to your user
   home-manager.users.__USERNAME__ = {
     home.stateVersion = "24.11";
-    programs.zsh.enable = true;
-    programs.git.enable = true;
-    programs.kitty.enable = true; # example terminal
-    programs.starship.enable = true;
 
-    # Some KDE-friendly basics
+    # --------- Packages (user scope) ---------
     home.packages = with pkgs; [
-      kate dolphin konsole
-      firefox
-      vlc
+      # KDE desktop apps
+      konsole dolphin kate okular gwenview spectacle ark
+
+      # Browsing / media
+      firefox vlc
+
+      # Dev / ops toolbelt
+      gh ripgrep fd bat jq yq tree fzf eza
+      kubectl k9s helm
+      awscli2 azure-cli
+      sops age
+      opentofu # switch to terraform if you prefer
+      ansible
+      # Handy utilities
+      ncdu duf
     ];
 
-    # Example dotfile bits
-    programs.git.userName = "__USERNAME__";
-    programs.git.userEmail = "you@example.com";
+    # --------- Shell, prompt, and tooling ---------
+    programs.zsh = {
+      enable = true;
+      autosuggestions.enable = true;
+      syntaxHighlighting.enable = true;
+      history.size = 50000;
+      shellAliases = {
+        ll = "eza -lah";
+        k = "kubectl";
+        g = "git";
+        tf = "tofu"; # opentofu alias
+      };
+    };
+
+    programs.starship.enable = true;
+
+    programs.direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+
+    programs.kitty = {
+      enable = true;
+      settings = {
+        confirm_os_window_close = 0;
+        enable_audio_bell = "no";
+      };
+    };
+
+    programs.vscode = {
+      enable = true;
+      userSettings = {
+        "editor.fontFamily" = "JetBrains Mono, monospace";
+        "editor.fontLigatures" = true;
+        "editor.formatOnSave" = true;
+        "files.trimTrailingWhitespace" = true;
+        "terminal.integrated.defaultProfile.linux" = "zsh";
+      };
+      extensions = with pkgs.vscode-extensions; [
+        ms-python.python
+        ms-vscode.cpptools
+        ms-azuretools.vscode-docker
+        github.vscode-github-actions
+        redhat.vscode-yaml
+        hashicorp.terraform
+      ];
+    };
+
+    programs.git = {
+      enable = true;
+      userName = "__USERNAME__";
+      userEmail = "you@example.com"; # update after first boot
+      extraConfig = {
+        init.defaultBranch = "main";
+        pull.rebase = true;
+        push.autoSetupRemote = true;
+        core.editor = "vim";
+      };
+    };
+
+    programs.ssh = {
+      enable = true;
+      extraConfig = ''
+        AddKeysToAgent yes
+        ServerAliveInterval 60
+      '';
+    };
+
+    services.gpg-agent = {
+      enable = true;
+      enableSshSupport = true;
+      defaultCacheTtl = 7200;
+      maxCacheTtl = 86400;
+    };
+
+    # --------- XDG & session defaults ---------
+    xdg.enable = true;
+    xdg.userDirs.enable = true;
+
+    home.sessionVariables = {
+      EDITOR = "vim";
+      VISUAL = "code";
+      PAGER = "bat";
+    };
   };
 }
